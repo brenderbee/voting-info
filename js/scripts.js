f359be6fc968b3cf13de5d7de40e7593da9cb234
Brenda Franco and Matt Miller <matt@>
2018-03-22 09:23:29 -0700

    add alert for 12 and under.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function(){
   var age = parseInt(prompt("How old are you?"));
 
   if (age >= 18) {
+    $("h1").hide();
     $("#of-age").fadeIn("slow");
+  } else if (age <= 12){
+    $("#minor").fadeIn("slow");
+    alert("What are you doing?! Go out and have some fun!");
   } else {
     $("#minor").fadeIn("slow");
   }
+
 });
